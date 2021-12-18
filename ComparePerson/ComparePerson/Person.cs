a406912af1a75f285203040cb03dc1f816b6832f
FarhadJA1 <farhadja@code.edu.az>
2021-12-18 14:11:07 +0400

    initial

---
+++ ComparePerson/ComparePerson/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComparePerson
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public static string operator >(Person person1, Person person2)
+        {
+            if(person1.Age > person2.Age)
+            {
+                return person1.Name;
+            }
+            else
+            {
+                return person2.Name;
+            }
+        }
+        public static string operator<(Person person1, Person person2)
+        {
+            if (person1.Age < person2.Age)
+            {
+                return person1.Name;
+            }
+            else
+            {
+                return person2.Name;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ComparePerson/ComparePerson/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ComparePerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Generics
+
+
+            #endregion
+
+
+            #region Operator Overloading
+
+            #endregion
+             Person person1 = new Person()
+            {
+                Name = "Farhad",
+
+                Surname = "Azadaliyev",
+
+                Age = 25
+
+                
+            };
+
+            Person person2 = new Person()
+            {
+                Name = "Tural",
+
+                Surname = "Zeynalli",
+
+                Age = 24
+
+            };
+
+            Person person3 = new Person()
+            {
+                Name = "Kamran",
+
+                Surname = "Mammadov",
+
+                Age = 21
+
+
+            };
+
+            Console.WriteLine(person1<person3);
+
+        }
+        
+    }
+}
